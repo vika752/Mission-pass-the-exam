a4e32ef029568ca5510e986eb0acc9cee84f329f
YankhotovDenny <65371997+YankhotovDenny@users.noreply.github.com>
2020-12-17 14:06:42 +0300

    Add files via upload

--- g.py
+++ g.py
@@ -2,28 +2,66 @@ import os
 import random
 import pygame, sys
 
+def load_image(name, color_key=None):
+    fullname = os.path.join('image', name)
+    image = pygame.image.load(fullname).convert()
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
 # Инициализация PyGame
 pygame.init()
 pygame.mixer.init()
+
 mab = 0
 # параметры окна игры
 height = 600
 width = 720
 size = height, width
 screen = pygame.display.set_mode(size)
+
 running = True
+
 s1 = []
 s2 = []
+
+f = open("config.txt", encoding="utf8")
+s = f.read().split()
+Presents = int(s[0])
+flag_hero = s[1]
+f.close()
 # создаем звуковой объект
 # воспроизводим его (фоновая музыка)
-#splat = pygame.mixer.Sound("image\fon.wav")
-#splat.set_volume(0.1)  # громкость фоновой музыки
-#present = pygame.mixer.Sound("image\present.wav")
-#present.set_volume(0.5)
-#jump = pygame.mixer.Sound("image\jump.wav")  # звук прыжка
-#jump.set_volume(0.2)
+# splat = pygame.mixer.Sound("image\fon.wav")
+# splat.set_volume(0.1)  # громкость фоновой музыки
+# present = pygame.mixer.Sound("image\present.wav")
+# present.set_volume(0.5)
+# jump = pygame.mixer.Sound("image\jump.wav")  # звук прыжка
+# jump.set_volume(0.2)
 
-#splat.play()
+# splat.play()
+
+
+'''Считывание ТОПа лидеров'''
+
+name_player = []
+coins = []
+
+with open('Statistics.txt') as input_file:
+    for line in input_file:
+        if len(line.strip()) == 0:
+            continue  # пустые строки и строки-комментарии пропускаем
+
+        tokens = line.split()
+        name_player.append(str(tokens[0]))
+        coins.append(str(tokens[1]))
+
+top = sorted([(name_player[i], coins[i]) for i in range(len(name_player))], key=lambda x: int(x[1]), reverse=True)
+
 
 '''
 создание класса, в котором прописывается сам студент(лыжник);
@@ -32,8 +70,25 @@ s2 = []
 
 
 class Person(pygame.sprite.Sprite):
-    image = pygame.Surface((50, 50))
-    image.fill((0, 255, 0))
+    if flag_hero == 'boy':
+        imag2 = load_image("boy2.png", -1)
+        imag2 = pygame.transform.scale(imag2, (imag2.get_width() // 6, imag2.get_height() // 6))
+
+        imag1 = load_image("boy1.png", -1)
+        imag1 = pygame.transform.scale(imag1, (imag1.get_width() // 6, imag1.get_height() // 6))
+
+        image = load_image("boye.png", -1)
+        image = pygame.transform.scale(image, (image.get_width() // 6, image.get_height() // 6))
+
+    else:
+        imag2 = load_image("girl2.png", -1)
+        imag2 = pygame.transform.scale(imag2, (imag2.get_width() // 6, imag2.get_height() // 6))
+
+        imag1 = load_image("girl1.png", -1)
+        imag1 = pygame.transform.scale(imag1, (imag1.get_width() // 6, imag1.get_height() // 6))
+
+        image = load_image("girle.png", -1)
+        image = pygame.transform.scale(image, (image.get_width() // 6, image.get_height() // 6))
 
     def __init__(self, pos):
         super().__init__(all_sprites)
@@ -52,27 +107,31 @@ class Person(pygame.sprite.Sprite):
 
     def jump(self):
         if self.nap and self.nap != 999:
-            self.Vx = -20
+            self.Vx = -15
             self.Vy = 18
             self.nap = 999
             self.z = False
+            self.image = Person.imag2
         elif self.nap != 999:
-            self.Vx = 20
+            self.Vx = 15
             self.Vy = 18
             self.nap = 999
             self.z = False
+            self.image = Person.imag1
 
     def update(self):
-        if self.rect.centerx <= 25 and self.z:
+        if self.rect.centerx <= 50 and self.z:
             self.nap = 0
             self.Vx = 0
             self.Vy = 0
-            self.rect.centerx = 25
-        elif self.rect.centerx >= 575 and self.z:
+            self.rect.centerx = 50
+            self.image = Person.image
+        elif self.rect.centerx >= 525 and self.z:
             self.nap = 1
-            self.rect.centerx = 575
+            self.rect.centerx = 525
             self.Vx = 0
             self.Vy = 0
+            self.image = Person.image
         if self.tick % 5 == 0:
             if self.Vy != 0:
                 self.Vy -= 2
@@ -96,8 +155,9 @@ class Person(pygame.sprite.Sprite):
 
 
 class Snowball(pygame.sprite.Sprite):
-    image = pygame.Surface((50, 50))
-    image.fill((255, 0, 0))
+    k = random.choice([3.4 + 0.2 * i for i in range(5)])
+    abst1 = load_image("abst.png", -1)
+    image = pygame.transform.scale(abst1, (int(abst1.get_width() / k), int(abst1.get_height() / k)))
 
     def __init__(self, x):
         super().__init__(all_sprites)
@@ -124,9 +184,56 @@ class Snowball(pygame.sprite.Sprite):
 '''
 
 
+def for_statistics(score):
+    global top
+    for i in range(0, len(name_player)):
+        if int(top[i][1]) < score:
+            run = True
+            name = ""
+            font = pygame.font.Font(None, 50)
+            while run:
+                all_sprites.draw(screen)
+                for evt in pygame.event.get():
+                    if evt.type == pygame.KEYDOWN:
+                        if evt.key == pygame.K_RETURN and name:
+                            last_record = top[i]
+                            top[i] = (name, score)
+                            for j in range(5):
+                                try:
+                                    last_record, top[i+j+1] = top[i+j+1], last_record
+                                except:
+                                    break
+                            f = open("Statistics.txt", 'w')
+                            for i in top:
+                                f.write(str(i[0]) + ' ' + str(i[1]) + '\n')
+                            f.close()
+                            return
+                        elif evt.unicode.isalpha():
+                            name += evt.unicode
+                        elif evt.key == pygame.K_BACKSPACE:
+                            name = name[:-1]
+                    elif evt.type == pygame.QUIT:
+                        exit()
+                font = pygame.font.Font(None, 50)
+                text = font.render("{}".format(str(name)), 1, (250, 50, 100))
+                screen.blit(text, (290, 100))
+
+                font = pygame.font.Font(None, 50)
+                text = font.render("{}".format(str(xxx.score)), 1, (250, 50, 100))
+                screen.blit(text, (290, 50))
+
+                font = pygame.font.Font(None, 50)
+                text = font.render("{}".format(str(Presents)), 1, (0, 0, 0))
+                screen.blit(text, (290, 680))
+
+                fpsClock.tick(FPS)
+                pygame.display.flip()
+                screen.fill('white')
+
+
 class Present(pygame.sprite.Sprite):
-    image = pygame.Surface((50, 50))
-    image.fill((255, 255, 0))
+    image = load_image("present.png", -1)
+    image = pygame.transform.scale(image, (image.get_width() // 3, image.get_height() // 3))
 
     def __init__(self, x):
         super().__init__(all_sprites)
@@ -136,31 +243,126 @@ class Present(pygame.sprite.Sprite):
         self.rect.centerx = x
         self.rect.centery = 0
         self.v = random.choice(range(7, 12))
-        self.rot = random.choice(range(360))
-        self.rv = random.choice(range(30))
 
     def update(self):
         self.rect.centery += self.v
-        self.rot += self.rv
-        self.image = pygame.transform.rotate(Present.image, self.rot)
         if self.rect.centery > 800:
             self.kill()
 
 
+# cоздаем класс меню
+class Menu:
+    def __init__(self, punkts=[400, 350, u'Punkt', (250, 250, 30), (250, 30, 250)]):
+        self.punkts = punkts
+        self.menu()
+
+        screen.fill((50, 50, 50))
+
+    # функция отрисовки всех пунктов меню (название пов-сти, шрифт, номер эл-та)
+    def render(self, pov, font, num_punkt):
+        # перебирает все элементы списка по пунктам
+        for i in self.punkts:
+            # условие, в к-м проверяется совп-ет ли номер текущ-го пункта с номером, к-й был передан функции
+            if num_punkt == i[5]:
+                pov.blit(font.render(i[2], 1, i[4]), (i[0], i[1] - 30))
+            else:
+                pov.blit(font.render(i[2], 1, i[3]), (i[0], i[1] - 30))
+
+    # функция, в к-й реализуется сцена меню
+    def menu(self):
+        window = pygame.display.set_mode((height, width))
+        pygame.font.init()
+        pygame.display.set_caption('menu')
+        screen = pygame.Surface((height, width))
+        pygame.key.set_repeat(1, 1)
+        done = True
+        font_menu = pygame.font.Font('font.ttf', 70)
+        pygame.key.set_repeat(0, 0)
+        pygame.mouse.set_visible(True)
+        punkt = 0
+        global flag_hero
+        while done:
+            screen.fill((0, 150, 200))
+            # блок управления мышью в меню
+            mp = pygame.mouse.get_pos()
+            # проверяем, не пересекается ли мышь с пунктом меню
+            for i in self.punkts:
+                if mp[0] > i[0] and mp[0] < i[0] + 155 and mp[1] > i[1] and mp[1] < i[1] + 50:
+                    punkt = i[5]
+            self.render(screen, font_menu, punkt)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    # блок перемещения по меню, с помощью клавиш
+                    if event.key == pygame.K_UP:
+                        if punkt > 0:
+                            punkt -= 1
+                    if event.key == pygame.K_DOWN:
+                        if punkt < len(self.punkts) - 1:
+                            punkt += 1
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if punkt == 0:
+                        done = False
+                    elif punkt == 1:
+                        mini_punkts = [(260, 300, u'BOY', (11, 0, 77), (255, 255, 255), 4),
+                                       (255, 340, u'GIRL', (11, 0, 77), (255, 255, 255), 5)]
+                        mini_menu = Menu(mini_punkts)
+                    elif punkt == 2:
+                        mini2_punkts = [(260, 500, u'EXIT', (11, 0, 77), (255, 255, 255), 6),
+                                        (175, 300, name_player[0] + ' - ' + coins[0], (11, 0, 77), (255, 255, 255), 10),
+                                        (175, 340, name_player[1] + ' - ' + coins[1], (11, 0, 77), (255, 255, 255), 10),
+                                        (175, 380, name_player[2] + ' - ' + coins[2], (11, 0, 77), (255, 255, 255), 10),
+                                        (175, 420, name_player[3] + ' - ' + coins[3], (11, 0, 77), (255, 255, 255), 10),
+                                        (175, 460, name_player[4] + ' - ' + coins[4], (11, 0, 77), (255, 255, 255), 10)]
+                        Menu(mini2_punkts)
+                    elif punkt == 3:
+                        f = open("Statistics.txt", 'w')
+                        for i in top:
+                            f.write(str(i[0]) + ' ' + str(i[1]) + '\n')
+                        f.close()
+                        exit()
+                    elif punkt == 4:
+                        flag_hero = 'boy'
+                        done = False
+                    elif punkt == 5:
+                        flag_hero = 'girl'
+                        done = False
+                    elif punkt == 6:
+                        done = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        done = False
+
+            window.blit(screen, (0, 30))
+            pygame.display.flip()
+
+
+# Строки для работы меню
+punkts = [(260, 300, u'PLAY', (11, 0, 77), (255, 255, 255), 0),
+          (175, 340, u'CHOOSE THEME', (11, 0, 77), (255, 255, 255), 1),
+          (210, 380, u'STATISTICS', (11, 0, 77), (255, 255, 255), 2),
+          (260, 420, u'EXIT', (11, 0, 77), (255, 0, 0), 3)]
+menu = Menu(punkts)
+
 # создание группы спрайтов в игре
 all_sprites = pygame.sprite.Group()
 
+n1 = pygame.sprite.Sprite()
+n1.image = load_image("end1.png", -1)
+n1.rect = n1.image.get_rect()
+n1.rect.centerx, n1.rect.centery = 54, 250
+
+n2 = pygame.sprite.Sprite()
+n2.image = load_image("end2.png", -1)
+n2.rect = n2.image.get_rect()
+n2.rect.centerx, n2.rect.centery = 546, 250
+
+all_sprites.add(n1)
+all_sprites.add(n2)
+
 xxx = Person((25, 500))
 
-f = open("config.txt", encoding="utf8")
-try:
-    f = open("config.txt", encoding="utf8")
-    Presents = int(f.read())
-    f.close()
-except:
-    Presents = 0
-
-FPS = 40
+FPS = 60
 fpsClock = pygame.time.Clock()
 
 # цикл игры
@@ -170,8 +372,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            xxx.jump()
-            #jump.play()
+            if event.key == pygame.K_ESCAPE:
+                menu = Menu(punkts)
+            else:
+                xxx.jump()
+                # jump.play()
 
     if xxx.tick % xxx.tickrate == 0 and xxx.rect.centery > 150:
         if xxx.nap == 0:
@@ -221,6 +426,7 @@ while running:
                                 xxx.rect.centerx = 25
                                 xxx.rect.centery = 475
                             elif event.key == pygame.K_BACKSPACE:
+                                for_statistics(xxx.score)  # функция добавляющая результат в стистику, при взятии рекорда
                                 for g in s1:
                                     g.kill()
                                 for g in s2:
@@ -240,6 +446,7 @@ while running:
 
                     pygame.display.flip()
             else:
+                for_statistics(xxx.score)  # функция добавляющая результат в стистику, при взятии рекорда
                 for g in s1:
                     g.kill()
                 for g in s2:
@@ -255,13 +462,17 @@ while running:
             s2.remove(i)
             i.kill()
             Presents += 1
-            #present.play()  # музыка начисления баллов
+            # present.play()  # музыка начисления баллов
             break
 
     all_sprites.draw(screen)
     # обновление
     all_sprites.update()
     xxx.z = True
+
+    for i in range(len(top)):
+        name_player[i] = str(top[i][0])
+        coins[i] = str(top[i][1])
 
     # отображение кол-ва баллов (в нижней части экрана)
     font = pygame.font.Font(None, 50)
@@ -272,13 +483,12 @@ while running:
     font = pygame.font.Font(None, 50)
     text = font.render("{}".format(str(Presents)), 1, (0, 0, 0))
     screen.blit(text, (290, 680))
-
     xxx.tick += 1
     fpsClock.tick(FPS)
     pygame.display.flip()
 
 f = open("config.txt", 'w')
-f.write(str(Presents))
+f.write(str(Presents) + ' ' + flag_hero)
 f.close()
 
 pygame.quit()
